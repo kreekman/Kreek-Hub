b179d4be0a2fcf586e84871ddad45b863e22af3a
kreekman <118077319+kreekman@users.noreply.github.com>
2023-08-22 17:18:48 -0400

    Update and rename [Loadstring] KreekLoader.lua to [Loadstring] PLSPLAY.lua

--- [Loadstring] KreekLoader.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kreekman/Kreek-Hub/KreekLoader/%5BObfuscated%5D%20KreekLoader.lua"))()
---
+++ [Loadstring] PLSPLAY.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kreekman/Kreek-Hub/PLSPLAY/%5BObfuscated%5D%20PLSPLAY.lua"))()
